c77d0b692d5ee3067bc22305ec3ea70670323148
NihalNazim33582 <70530227+NihalNazim33582@users.noreply.github.com>
2020-11-22 16:11:21 -0500

    Add files via upload

---
+++ Golden Project/Golden Project/Chest.js
@@ -0,0 +1,13 @@
+class Chest{
+    constructor(x,y){
+        this.x =x;
+        this.y =y;
+
+        this.body = createSprite(x, y, 10, 10);
+        this.image = loadImage("Pictures/chest.png");
+        this.body.addImage(this.image);
+        this.body.scale = 0.2;
+        World.add(world,this.body);
+    }
+
+}
